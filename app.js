d24e8d6512e963f11e280add5893bfce30cd515d
Avinash Kumar <59990479+avinashkumar-github@users.noreply.github.com>
2021-06-20 20:26:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require("express")();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Welcome to the node app!"
+    message: "Welcome to the node app!!"
   });
 });
 let PORT = process.env.PORT || 3000;
