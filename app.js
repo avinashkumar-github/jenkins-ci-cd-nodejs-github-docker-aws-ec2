4f55c3eeb17d47908e2af5df9dbb8dd995cd6922
Kumar <avinash.kumar11@sap.com>
2021-06-20 19:36:45 +0530

    Basic node app

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the node app"
+  });
+});
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server started!!");
+});
